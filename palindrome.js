ed166cf4d84627b2c1d093aafec98a9901a0a78f
Karstek <karstek@users.noreply.github.com>
2020-07-31 17:03:02 -0300

    My first algorithms

---
+++ palindrome.js
@@ -0,0 +1,11 @@
+//Returns true if provided string is a palindrome. Removes all punctuation, spaces and symbols before comparison.
+function palindrome(str) {
+    let str2 = "";
+    let regex = /[\W_]/g;
+
+    str = str.replace(regex, "");
+    str = str.toLowerCase();
+    str2 = str.split("").reverse().join("");
+
+    return str === str2;
+}
