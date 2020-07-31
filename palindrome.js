9b983a3bf725f9a77b57548cf1b885519f0402df
Karstek <karstek@users.noreply.github.com>
2020-07-31 19:06:11 -0300

    Indentation fix and added an example

--- palindrome.js
+++ palindrome.js
@@ -1,11 +1,13 @@
 //Returns true if provided string is a palindrome. Removes all punctuation, spaces and symbols before comparison.
 function palindrome(str) {
-    let str2 = "";
-    let regex = /[\W_]/g;
+  let str2 = "";
+  let regex = /[\W_]/g;
 
-    str = str.replace(regex, "");
-    str = str.toLowerCase();
-    str2 = str.split("").reverse().join("");
+  str = str.replace(regex, "");
+  str = str.toLowerCase();
+  str2 = str.split("").reverse().join("");
 
-    return str === str2;
+  return str === str2;
 }
+//EXAMPLE
+palindrome("hannah");
